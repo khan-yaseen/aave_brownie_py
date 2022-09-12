3f84d8aa3972ac34d1e1691d3df9e635bb988a0b
yaseen <yaseenkhan@61yahoo.com>
2022-09-12 17:32:24 +0530

    function to get WETH(wrapped ethereum) by depositing ETH

---
+++ scripts/get_weth.py
@@ -0,0 +1,20 @@
+from scripts.helpful_scritps import get_account
+from brownie import interface, config, network
+from web3 import Web3
+
+def main():
+    get_weth()
+
+
+def get_weth():
+    """
+    Mints WETH bt depositing ETH.
+    """
+    # ABI
+    # Address
+    account = get_account()
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    wei = Web3.toWei(0.000001, "ether")
+    tx = weth.deposit({"from": account, "value": wei})
+    print("REceived 0.000001 WETH")
+    return tx
