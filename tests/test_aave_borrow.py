9a9a254d2a63bf38e411241ad77c20dc34d115ca
yaseen <yaseenkhan@61yahoo.com>
2022-09-14 18:53:52 +0530

    test approve_erc20

--- tests/test_aave_borrow.py
+++ tests/test_aave_borrow.py
@@ -1,4 +1,5 @@
-from scripts.aave_borrow import get_asset_price, get_lending_pool
+from scripts.aave_borrow import get_asset_price, get_lending_pool, approve_erc20
+from scripts.helpful_scritps import get_account
 from brownie import config, network
 
 
@@ -16,4 +17,15 @@ def test_get_lending_pool():
     lending_pool = get_lending_pool()
     # Assert
     assert lending_pool is not None
-    
+
+
+def test_approve_erc20():
+    # Arrange
+    account = get_account()
+    lending_pool = get_lending_pool()
+    amount = 1000000000000000000  # 1
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    # Act
+    tx = approve_erc20(amount, lending_pool.address, erc20_address, account)
+    # Assert
+    assert tx is not True
